26751c16c1bb77c4e1a5d7e3c4fe551dbac8470f
Bektur <sultanov_b@auca.kg>
2023-12-25 14:45:09 +0600

    Add button and random.

--- app/src/main/java/com/example/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -1,14 +1,30 @@
 package com.example.dicerollerapp
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.dicerollerapp.ui.theme.DiceRollerAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,7 +46,42 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
+    val context = LocalContext.current
 
+    var dice by remember {
+        mutableIntStateOf(1)
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Text(
+                modifier = Modifier.padding(bottom = 50.dp),
+                fontSize = 36.sp,
+                text = "$dice"
+            )
+
+            Button(onClick = {
+                showToast(context)
+                dice = (1..6).random()
+            }) {
+                Text(
+                    fontSize = 24.sp,
+                    text = stringResource(id = R.string.roll).uppercase()
+                )
+            }
+        }
+    }
+}
+
+fun showToast(context: Context) {
+    Toast.makeText(context, "Dice Rolled", Toast.LENGTH_SHORT).show()
 }
 
 @Preview(showBackground = true)
